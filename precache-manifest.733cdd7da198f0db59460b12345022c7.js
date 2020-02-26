95377252524c3e13ec96820bde36e882b166f915
jimmyhwang4 <35846105+jimmyhwang4@users.noreply.github.com>
2020-02-26 00:33:36 -0500

    Updates

---
+++ precache-manifest.733cdd7da198f0db59460b12345022c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0e39a1937c806dab94b3e2c0da1a816",
+    "url": "/Dynamic-Web-Exercise-One/index.html"
+  },
+  {
+    "revision": "f3f57d5efaba52d8f64f",
+    "url": "/Dynamic-Web-Exercise-One/static/css/main.76cf24c4.chunk.css"
+  },
+  {
+    "revision": "4c2542eedafbe54ceda0",
+    "url": "/Dynamic-Web-Exercise-One/static/js/2.b75d4dea.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Dynamic-Web-Exercise-One/static/js/2.b75d4dea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3f57d5efaba52d8f64f",
+    "url": "/Dynamic-Web-Exercise-One/static/js/main.8043227f.chunk.js"
+  },
+  {
+    "revision": "d0d8e1f17f4c8ecee51f",
+    "url": "/Dynamic-Web-Exercise-One/static/js/runtime-main.2881e76b.js"
+  }
+]);
